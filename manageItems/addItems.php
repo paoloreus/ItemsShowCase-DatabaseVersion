08568c24dc6ef6e9800f72d93b36f5d2c1cf4f20
Videospirit <73370893+Videospirit@users.noreply.github.com>
2020-12-09 22:29:00 -0500

    Update addItems.php

--- manageItems/addItems.php
+++ manageItems/addItems.php
@@ -8,13 +8,35 @@ include __DIR__ . '/items.php';
 $item = new Items();
 
 if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'add'){
-$query = sprintf("INSERT INTO %s (name, description, price, category, image) VALUES ('%s', '%s', '%0.2f', '%s', '%s')",
-Items::$table_name, $_REQUEST['name'], $_REQUEST['description'], $_REQUEST['price'], $_REQUEST['category'], $_FILES['image']['name']);
+    $count=0;
+    $count2=1;
+    $imageString='';
+    $imageString2='';
 
-$item->query($query);
-    $name_dir = "../images/";
-    $name_file = $name_dir . basename($_FILES['image']['name']);
-    move_uploaded_file($_FILES['image']['tmp_name'], $name_file);
+    //Generates 2 strings for adding images to the query.
+    foreach ($_FILES['image'] as $image){
+        if(!$_FILES['image']['name'][$count]=='') {
+            $imageString .= ", image";
+            if ($count2 > 1) {
+                $imageString .= $count2;
+                $imageString2 .= "', '";
+            }
+            $imageString2 .= $_FILES['image']['name'][$count];
+            $name_file = "../images/" . basename($_FILES['image']['name'][$count]);
+            move_uploaded_file($_FILES['image']['tmp_name'][$count], $name_file);
+            $count2++;
+        }
+        $count++;
+    }
+    if($imageString==''){
+        $imageString = ", image";
+        $imageString2= "default.png";
+    }
+    $query = sprintf("INSERT INTO %s (name, description, price, category$imageString) VALUES ('%s', '%s', '%0.2f', '%s', '%s')",
+        Items::$table_name, $_REQUEST['name'], $_REQUEST['description'], $_REQUEST['price'], $_REQUEST['category'],$imageString2);
+
+    $item->query($query);
+
 header('Location: ../public/indexAdmin.php');
 
 }
@@ -33,7 +55,11 @@ header('Location: ../public/indexAdmin.php');
     Price: <input type="text" name="price"><br>
     Category: <input type="text" name="category"><br>
     Description: <br><textarea name="description"></textarea> <br>
-    Image: <input type="file" name="image"><br><br>
+    Image: <input type="file" name="image[]"><br>
+    Image2: <input type="file" name="image[]"><br>
+    Image3: <input type="file" name="image[]"><br>
+    Image4: <input type="file" name="image[]"><br>
+    Image5: <input type="file" name="image[]"><br><br>
     <input type="submit" value="Add New Item">
     <button type="submit" formaction="../public/indexAdmin.php">Back to Home</button>
 </form>
